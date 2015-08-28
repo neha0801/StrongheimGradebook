05d56770629be8f2114aef1ab54188d172d5b230
neha0801 <ns2617@nyu.edu>
2015-08-28 16:33:10 -0400

    Create Gradebook.sql

---
+++ StrongheimGradebook/Gradebook.sql
@@ -0,0 +1,25 @@
+--------------------------------------------------------
+--  File created - Friday-August-28-2015   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Table GRADEBOOK
+--------------------------------------------------------
+
+  CREATE TABLE "TESTDB"."GRADEBOOK" 
+   (	"ASSSIGNMENT" VARCHAR2(255 BYTE), 
+	"GRADE" NUMBER
+   ) SEGMENT CREATION IMMEDIATE 
+  PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 NOCOMPRESS LOGGING
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1 BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)
+  TABLESPACE "USERS" ;
+REM INSERTING into TESTDB.GRADEBOOK
+SET DEFINE OFF;
+Insert into TESTDB.GRADEBOOK (ASSSIGNMENT,GRADE) values ('fdshgfd',100);
+Insert into TESTDB.GRADEBOOK (ASSSIGNMENT,GRADE) values ('djgfjdhgklf',100);
+Insert into TESTDB.GRADEBOOK (ASSSIGNMENT,GRADE) values ('gfdxhnf',10);
+Insert into TESTDB.GRADEBOOK (ASSSIGNMENT,GRADE) values ('assignment',90);
+Insert into TESTDB.GRADEBOOK (ASSSIGNMENT,GRADE) values ('2323',22);
+Insert into TESTDB.GRADEBOOK (ASSSIGNMENT,GRADE) values ('gfdgh',100);
+Insert into TESTDB.GRADEBOOK (ASSSIGNMENT,GRADE) values ('gfshgf',100);
+Insert into TESTDB.GRADEBOOK (ASSSIGNMENT,GRADE) values ('htcfjyhj',100);
